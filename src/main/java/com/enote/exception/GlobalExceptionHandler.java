2447c25de3712cdc51fada38959d4cbac849027c
Rushi282 <rushimajale5453@gmail.com>
2024-12-27 10:28:52 +0530

    file download impl

--- src/main/java/com/enote/exception/GlobalExceptionHandler.java
+++ src/main/java/com/enote/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.enote.exception;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -23,5 +26,17 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(ResourceAlreadyExistException.class)
 	public ProblemDetail handleResourceAlreadyExistException(ResourceAlreadyExistException e) {
 		return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, e.getMessage());
+	}
+	
+//	IOException
+	@ExceptionHandler(IOException.class)
+	public ProblemDetail handleIOException(IOException e) {
+		return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+	}
+	
+//	FileNotFoundException
+	@ExceptionHandler(FileNotFoundException.class)
+	public ProblemDetail handleFileNotFoundException(FileNotFoundException e) {
+		return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
 	}
 }
